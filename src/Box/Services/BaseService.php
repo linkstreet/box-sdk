1007182790bfc76a8942ea4bf7944856b8328730
Cerlin <cerlin@linkstreet.in>
2016-12-13 15:21:58 +0530

    Added `getAuthHeaders` method to base service so that all the services can access the auth header.

--- src/Box/Services/BaseService.php
+++ src/Box/Services/BaseService.php
@@ -3,13 +3,25 @@
 namespace Box\Services;
 
 use GuzzleHttp\Client as GuzzleClient;
+use Box\Auth\AppAuth;
 
 class BaseService
 {
     protected $guzzle_client;
 
-    public function __construct()
+    protected $app_auth;
+
+    public function __construct(AppAuth $app_auth)
     {
         $this->guzzle_client = new GuzzleClient();
+
+        $this->app_auth = $app_auth;
+    }
+
+    protected function getAuthHeaders()
+    {
+        return [
+            "Authorization" => "Bearer " . $this->app_auth->getTokenInfo()->access_token
+        ];
     }
 }
