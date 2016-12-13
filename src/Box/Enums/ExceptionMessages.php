e882b7742f747e429191547773868fb9a67a8af8
Cerlin <cerlin@linkstreet.in>
2016-12-13 15:21:03 +0530

    Added Exception messages

---
+++ src/Box/Enums/ExceptionMessages.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Box\Enums;
+
+/**
+ * Class ExceptionMessages
+ * @package Box\Enums
+ */
+abstract class ExceptionMessages
+{
+    /**
+     * Error message for file not found exception
+     */
+    const FILENOTFOUND = "File is not found or file not readable ";
+}
